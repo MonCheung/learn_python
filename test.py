a9b9d536585bffb3c8c74a81c1c8d5ff76fa0f19
张哲豪 <zhehao@meizu.com>
2018-09-18 17:29:38 +0800

    use property to class

--- test.py
+++ test.py
@@ -587,7 +587,7 @@ else:
         print('测试成功!')
 '''
 
-
+'''
 #实例属性和类属性
 # -*- coding: utf-8 -*-
 class Student(object):
@@ -611,3 +611,38 @@ else:
         else:
             print('Students:', Student.count)
             print('测试通过!')
+'''
+
+
+#property属性在类中的应用
+# -*- coding: utf-8 -*-
+class Screen(object):
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self,value):
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self,value):
+        self._height = value
+
+    @property
+    def resolution(self):
+        return self._width*self._height
+
+# 测试:
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution = ', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
