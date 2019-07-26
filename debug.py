7b1f436dd97580473abeff4f370167b145e42ca7
张哲豪 <zhehao@meizu.com>
2019-07-26 15:08:40 +0800

    test debug.py

--- debug.py
+++ debug.py
@@ -1,15 +1,17 @@
-import time, functools
-def metric(fn):
-	@functools.wraps(fn)
-	def wrapper(*args,**kw):
-		print('%s executed in %s ms' % (fn.__name__, 10.24))
-		return fn(*args,**kw)
-	return wrapper
-	
-# 测试
-@metric
-def fast(x, y):
-    time.sleep(0.0012)
-    return x + y;
-f = fast(11, 22)
-f = fast(11, 22)
+class Student(object):
+
+    @property
+    def birth(self):
+        return self._birth
+
+    @birth.setter
+    def birth(self, value):
+        self._birth = value
+
+    @property
+    def age(self):
+        return 2019 - self._birth
+
+s = Student()
+s.birth = 1990
+print("age is",s.age)
