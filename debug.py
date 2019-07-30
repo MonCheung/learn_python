3364606713de20881556426bbb305714c025e367
张哲豪 <zhehao@meizu.com>
2019-07-30 18:03:52 +0800

    Unit testing

--- debug.py
+++ debug.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 import unittest
+
 class Student(object):
     def __init__(self, name, score):
         self.name = name
         self.score = score
     def get_grade(self):
-        if self.score >= 60:
-            return 'B'
+        n=range(101)
+        if self.score not in n:
+            raise ValueError
         if self.score >= 80:
             return 'A'
+        elif self.score >= 60:
+            return 'B'
         return 'C'
 
 class TestStudent(unittest.TestCase):
--- test.py
+++ test.py
@@ -648,7 +648,7 @@ else:
     print('测试失败!')
 '''
 
-
+'''
 #错误处理
 # -*- coding: utf-8 -*-
 from functools import reduce
@@ -657,7 +657,6 @@ def str2num(s):
     try:
         return int(s)
     except Exception as e:
-        print('',e)
         return float(s)
 
 def calc(exp):
@@ -672,3 +671,54 @@ def main():
     print('99 + 88 + 7.6 =', r)
 
 main()
+'''
+
+
+#单元测试练习
+# -*- coding: utf-8 -*-
+import unittest
+
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def get_grade(self):
+        n=range(101)
+        if self.score not in n:
+            raise ValueError
+        if self.score >= 80:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        return 'C'
+
+class TestStudent(unittest.TestCase):
+
+    def test_80_to_100(self):
+        s1 = Student('Bart', 80)
+        s2 = Student('Lisa', 100)
+        self.assertEqual(s1.get_grade(), 'A')
+        self.assertEqual(s2.get_grade(), 'A')
+
+    def test_60_to_80(self):
+        s1 = Student('Bart', 60)
+        s2 = Student('Lisa', 79)
+        self.assertEqual(s1.get_grade(), 'B')
+        self.assertEqual(s2.get_grade(), 'B')
+
+    def test_0_to_60(self):
+        s1 = Student('Bart', 0)
+        s2 = Student('Lisa', 59)
+        self.assertEqual(s1.get_grade(), 'C')
+        self.assertEqual(s2.get_grade(), 'C')
+
+    def test_invalid(self):
+        s1 = Student('Bart', -1)
+        s2 = Student('Lisa', 101)
+        with self.assertRaises(ValueError):
+            s1.get_grade()
+        with self.assertRaises(ValueError):
+            s2.get_grade()
+
+if __name__ == '__main__':
+    unittest.main()
