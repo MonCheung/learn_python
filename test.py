9eb51169deb65af6575a6883f441d68f9df888fd
张哲豪 <zhehao@meizu.com>
2019-08-22 16:37:32 +0800

    use sqlite

--- test.py
+++ test.py
@@ -1052,6 +1052,7 @@ print('ok')
 '''
 
 
+'''
 #利用urllib读取JSON，将JSON解析为Python对象
 # -*- coding: utf-8 -*-
 from urllib import request
@@ -1067,3 +1068,44 @@ data = fetch_data(URL)
 print(data)
 assert data['query']['results']['channel']['location']['city'] == 'Beijing'
 print('ok')
+'''
+
+
+
+#sqlite操作
+# -*- coding: utf-8 -*-
+import os
+import sqlite3
+
+db_file = os.path.join(os.path.dirname(__file__), 'test.db')
+if os.path.isfile(db_file):
+    os.remove(db_file)
+
+# 初始数据:
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
+cursor.execute('create table user(id varchar(20) primary key, name varchar(20), score int)')
+cursor.execute(r"insert into user values ('A-001', 'Adam', 95)")
+cursor.execute(r"insert into user values ('A-002', 'Bart', 62)")
+cursor.execute(r"insert into user values ('A-003', 'Lisa', 78)")
+cursor.close()
+conn.commit()
+conn.close()
+
+def get_score_in(low, high):
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
+    #执行查询语句并且排序
+    cursor.execute('select * from user where score between ? and ? order by score',(low,high))
+    values = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return [value[1] for value in values]
+
+
+# 测试:
+assert get_score_in(80, 95) == ['Adam'], get_score_in(80, 95)
+assert get_score_in(60, 80) == ['Bart', 'Lisa'], get_score_in(60, 80)
+assert get_score_in(60, 100) == ['Bart', 'Lisa', 'Adam'], get_score_in(60, 100)
+
+print('Pass')
