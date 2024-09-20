f3ef894d4a39def6f8c141828a1a248e1dd645df
MonCheung <monstar@qq.com>
2024-09-20 17:02:20 +0800

    'add pytest_compress_str'

---
+++ algorithm/pytest_compress_str.py
@@ -0,0 +1,23 @@
+import pytest
+from compress_str import CompressString
+
+#实例化导入的CompressString类
+compressor = CompressString()
+
+def test_compress_none():
+    assert compressor.compress(None) == None
+
+def test_compress_empty_string():
+    assert compressor.compress('') == ''
+
+def test_compress_no_reduction():
+    assert compressor.compress('AABBCC') == 'AABBCC'
+
+def test_compress_with_reduction():
+    assert compressor.compress('AAABCCDDDDE') == 'A3BC2D4E'
+    assert compressor.compress('BAAACCDDDD') == 'BA3C2D4'
+    assert compressor.compress('AAABAACCDDDD') == 'A3BA2C2D4'
+
+if __name__ == '__main__':
+    #指定文件路径，只执行当前测试脚本
+    pytest.main(['algorithm/pytest_compress_str.py'])
